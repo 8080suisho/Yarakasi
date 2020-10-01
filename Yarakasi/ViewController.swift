bfa4037ae4979cae18cecdd9340b21381d8b5031
Mizuki Morohoshi <8080suisho@gmail.com>
2020-10-01 23:46:53 +0900

    Initial Commit

---
+++ Yarakasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yarakasi
+//
+//  Created by 諸星水晶 on 2020/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
